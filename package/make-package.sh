90a8575c7b01eedf84bfc3311f270fdc7d9a052d
Robert Kaiser <kairo@kairo.at>
2013-07-21 17:19:54 +0200

    update version generation and developer def in mini manifest

--- package/make-package.sh
+++ package/make-package.sh
@@ -7,7 +7,7 @@ origdir=`pwd`
 
 rm -v $pkgdir/*
 zip -r -D -0 $pkgdir/package.zip * -x package/*
-dver=`date +%y.%j.%k`
+dver=`date +%y.%j.%H`
 pkgsize=`wc -c $pkgdir/package.zip | awk '{ print $1 }'`
 sed -e "s/DVER/$dver/" -e "s/PKGSIZE/$pkgsize/" package/manifest.package > $pkgdir/manifest.package
 cp package/.htaccess $pkgdir/
