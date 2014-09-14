f8e4d105515a751dba04ccca79323aaab592bca4
Robert Kaiser <kairo@kairo.at>
2014-09-14 22:27:07 +0200

    try adding a version into the manifest

--- package/make-package.sh
+++ package/make-package.sh
@@ -10,4 +10,9 @@ zip -r -D -0 $pkgdir/package.zip * -x package/{,.}*
 dver=`date +%y.%j.%H`
 pkgsize=`wc -c $pkgdir/package.zip | awk '{ print $1 }'`
 sed -e "s/DVER/$dver/" -e "s/PKGSIZE/$pkgsize/" package/manifest.package > $pkgdir/manifest.package
+sed -e "s/DVER/$dver/" -e "s/PKGSIZE/$pkgsize/" manifest.webapp > $pkgdir/manifest.webapp
+cd $pkgdir
+zip -f -0 package.zip manifest.webapp
+rm manifest.webapp
+cd $origdir
 cp package/.htaccess $pkgdir/
