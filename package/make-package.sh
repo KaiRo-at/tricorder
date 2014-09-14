eb77302534dbaf47f7962f7b25d159ec04c0bb26
Robert Kaiser <kairo@kairo.at>
2014-09-14 22:30:14 +0200

    don't package appcache

--- package/make-package.sh
+++ package/make-package.sh
@@ -6,7 +6,7 @@ cd ..
 origdir=`pwd`
 
 rm -v $pkgdir/*
-zip -r -D -0 $pkgdir/package.zip * -x package/{,.}*
+zip -r -D -0 $pkgdir/package.zip * -x package/{,.}* *.appcache
 dver=`date +%y.%j.%H`
 pkgsize=`wc -c $pkgdir/package.zip | awk '{ print $1 }'`
 sed -e "s/DVER/$dver/" -e "s/PKGSIZE/$pkgsize/" package/manifest.package > $pkgdir/manifest.package
